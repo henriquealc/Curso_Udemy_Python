608fbd7930f9051eefa8930fbb53376044655b6d
Henrique <aaluiziohenrique22@gmail.com>
2023-04-13 16:53:45 -0300

    Calculadora_V1.0
    Exercício do Curso de Python Udemy calculadora

--- aula40_Ex_Calculadora.py
+++ aula40_Ex_Calculadora.py
@@ -1,22 +1,36 @@
 '''
 Calculadora com while
 '''
-resultado = 0
 
 while True:
+    
+    print('{:^30}'.format('---------- CALCULADORA ----------'))
+    
     num = int(input('Insira o valor: '))
     num2 = int(input('Insira o outro valor: '))
-    calculo = input('Qual calculo deseja fazer? \nMultiplicação- (*) \nAdição- (+)...\n')
-    sair = input('Deseja sair [s]im? ').lower().startswith('s')    
+    print('='*35)
+    calculo = input('Informe o operador que deseja (+,*,/,-): ')
     
+    if calculo not in '+''-''/''*''-':
+        print('Insira uma opção válida.')
+    elif calculo == '+':
+        resultado = num + num2
+        print(resultado)
+    
+    elif calculo == '-':
+        resultado = num - num2
+        print(resultado)
+    
+    elif calculo == '*':
+        resultado = num * num2
+        print(resultado)
+    
+    elif calculo == '/':
+        resultado == '/'
+        print(resultado)
+    
+    sair = input('Deseja sair [s]im? ').lower().startswith('s')    
     if sair is True:
         break
-    else:
-        if calculo == '+':
-            resultado = num + num2
-        print(resultado)
-        if calculo == '-':
-            resultado1 = num - num2
-        print(resultado1)
-
+print('Obtigado por usar nossa calculadora, até mais...')
 
