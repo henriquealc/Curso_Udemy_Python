190ae370692b559793c5c3a70526c202c5ec8705
Henrique <aaluiziohenrique22@gmail.com>
2023-05-04 17:03:20 -0300

    for em lista

--- aula48_Intro_lista_mutáveis.py
+++ aula48_Intro_lista_mutáveis.py
@@ -103,10 +103,10 @@ Cuidados com dados mutáveis
 = - aponta para o mesmo valor na memória (mutável)
 '''
 
-lista_a = ['Luiz', 'Maria', 1, True, 1.2]
+# lista_a = ['Luiz', 'Maria', 1, True, 1.2]
 
-lista_b = lista_a.copy() # Copia a lista 'a'
+# lista_b = lista_a.copy() # Copia a lista 'a'
 
-lista_a[0] = 'Qualquer coisa' # Substitui o primeiro item da lista por 'Qualquer coisa'
-print(lista_a)
-print(lista_b)
+# lista_a[0] = 'Qualquer coisa' # Substitui o primeiro item da lista por 'Qualquer coisa'
+# print(lista_a)
+# print(lista_b)
---
+++ aula49_for_in_lista.py
@@ -0,0 +1,7 @@
+'''
+for in com listas
+'''
+lista = ['Maria', 'Helena', 'Luiz'] 
+
+for nome in lista:  # para cada nome em lista 
+    print(nome, type(nome)) # imprimi os nome da lista e seu tipo
