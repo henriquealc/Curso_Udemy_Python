e855d06cace8f46b8ed4a31078ae1e04ef0492ea
Henrique <aaluiziohenrique22@gmail.com>
2023-05-04 17:31:12 -0300

    For in lista, e exercício sobre
    Exemplo de 'for in' em lista e 2 soluções diferentes de exercício

--- aula49_for_in_lista.py
+++ aula49_for_in_lista.py
@@ -1,7 +1,33 @@
 '''
 for in com listas
 '''
-lista = ['Maria', 'Helena', 'Luiz'] 
+# lista = ['Maria', 'Helena', 'Luiz'] 
 
-for nome in lista:  # para cada nome em lista 
-    print(nome, type(nome)) # imprimi os nome da lista e seu tipo
+# for nome in lista:  # para cada nome em lista 
+#     print(nome, type(nome)) # imprimi os nome da lista e seu tipo
+
+
+'''
+Exercício proposto pelo professor:
+Exiba os índices da lista
+Solução feira por mim 
+'''
+# num = -1
+# lista = ['Maria', 'Helena', 'Luiz']
+# lista.append('João')
+
+# for nome in lista:
+#     num += 1
+#     print(num, nome, type(nome))
+
+
+
+'''Solução feita pelo professor'''
+
+lista = ['Mario', 'José', 'Kvaratskhelia']
+lista.append('James')
+
+indices = range(len(lista))
+
+for indice in indices:
+    print(indice, lista[indice], type(lista[indice]))
