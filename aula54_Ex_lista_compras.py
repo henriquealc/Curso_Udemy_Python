4c528b753b80a00ea44c5a7013ca561c19908244
Henrique <aaluiziohenrique22@gmail.com>
2023-05-05 18:49:35 -0300

    Exercício listas de compras
    Exercício usando listas para fazer uma lista de compras

---
+++ aula54_Ex_lista_compras.py
@@ -0,0 +1,92 @@
+"""
+Faça uma lista de comprar com listas
+O usuário deve ter a possibilidade de
+inserir, apagar e listar valores da sua lista
+Não permita que o programa quebre com 
+erros de índices inexistentes na lista.
+"""
+
+'''RESOLUÇÃO FEITA POR MIM'''
+#import os
+lista = [] # Sempre criar a variavel lista fora da estrutura de repetição
+num = -1
+
+while True:
+    opcao = input('Selecione uma opção: \n[i]nserir [a]apagar [l]istar s[air]: ').lower()
+    if opcao == 'i':
+        #os.system('clear')
+        inserir = input('Valor: ')
+        lista.append(inserir)
+    elif opcao == 'a':
+        #os.system('clear')
+        apagar = input('Digite o indice que deseja apagar: ')
+        
+        try:
+            indice = int(apagar)
+            del lista[indice] # deletar um indice da lista
+        
+         # informa o erro
+        except ValueError:
+            print('Por favor digite número int.')
+        except IndexError:
+            print('Índice não existe na lista')
+        except Exception:
+            print('Erro desconhecido')
+    elif opcao == 'l':
+        #os.system('clear')
+        print(lista) # Informa a lista e os itens que contém nela
+        
+        if len(lista) == 0:
+            print('Nada para listar')
+        
+        # mostra os indices da lista
+        for inserir in lista: 
+            num +=1
+            print(num, inserir)
+    elif opcao == 's':
+        print('Programa encerrado com sucesso, Obrigado!')
+        break
+    else:
+        print('Por favor escolha i, a ou l.')
+
+
+
+
+# Resolução feita pelo professor
+
+'''import os
+
+lista = []
+
+while True:
+    print('Selecione uma opção')
+    opcao = input('[i]nserir [a]pagar [l]istar: ')
+
+    if opcao == 'i':
+        os.system('clear')
+        valor = input('Valor: ')
+        lista.append(valor)
+    elif opcao == 'a':
+        indice_str = input(
+            'Escolha o índice para apagar: '
+        )
+
+        try:
+            indice = int(indice_str)
+            del lista[indice]
+        except ValueError:
+            print('Por favor digite número int.')
+        except IndexError:
+            print('Índice não existe na lista')
+        except Exception:
+            print('Erro desconhecido')
+    elif opcao == 'l':
+        os.system('clear')
+
+        if len(lista) == 0:
+            print('Nada para listar')
+
+        for i, valor in enumerate(lista):
+            print(i, valor)
+    else:
+        print('Por favor, escolha i, a ou l.')'''
