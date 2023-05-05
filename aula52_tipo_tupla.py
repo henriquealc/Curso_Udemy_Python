0b6163fd185aa9b5d5e225c706b602cbdfff78e6
Henrique <aaluiziohenrique22@gmail.com>
2023-05-05 18:47:41 -0300

    Tuplas em Python
    Alguns exemplos de como usar tuplas

---
+++ aula52_tipo_tupla.py
@@ -0,0 +1,11 @@
+'''
+Tupla - Quando não preciso mudar nem adicionar nenhum valor
+Tipo tupla - Uma lista imutável
+imutável - Quando não mudar e nem precisa adicionar algum valor
+'''
+nomes = 'Maria', 'Helena', 'Luiz' # Se não colocar os [] esta criando uma tupla 
+# nomes = ['Maria', 'Helena', 'Luiz'] # Isso é uma lista
+# nomes = tuple(nomes) # Transforma uma lista em tupla
+# nomes = list(nomes) # Transforma uma tupla em lista
+print(nomes[-1])
+print(nomes)
