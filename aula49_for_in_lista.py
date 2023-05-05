a0af9a7e5fc5b8cc198b4a4f60b5a04ec5f43b6c
Henrique <aaluiziohenrique22@gmail.com>
2023-05-05 16:11:34 -0300

    Alguns exemplos de como usar lista

--- aula49_for_in_lista.py
+++ aula49_for_in_lista.py
@@ -7,27 +7,3 @@ for in com listas
 #     print(nome, type(nome)) # imprimi os nome da lista e seu tipo
 
 
-'''
-Exercício proposto pelo professor:
-Exiba os índices da lista
-Solução feira por mim 
-'''
-# num = -1
-# lista = ['Maria', 'Helena', 'Luiz']
-# lista.append('João')
-
-# for nome in lista:
-#     num += 1
-#     print(num, nome, type(nome))
-
-
-
-'''Solução feita pelo professor'''
-
-lista = ['Mario', 'José', 'Kvaratskhelia']
-lista.append('James')
-
-indices = range(len(lista))
-
-for indice in indices:
-    print(indice, lista[indice], type(lista[indice]))
