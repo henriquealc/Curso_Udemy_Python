c2f9c919e8b3fd526d137742898bfcc81f47b41c
Henrique <aaluiziohenrique22@gmail.com>
2023-06-22 18:56:48 -0300

    Escopo de função e módulos globais

--- aula68_escopo_funcao_e_ modulos_global.py
+++ aula68_escopo_funcao_e_ modulos_global.py
@@ -5,16 +5,19 @@ Existe o escopo global e local.
 O escopo global é o escopo onde todo o código é alcançavel.
 O escopo local é o escopo onde apenas nomes do mesmo local
 podem ser alcançados.
+Não temos acessos a nomesde escopos internos nos escopos externos.
+A palavra global faz uma variável do escopo externo ser a
+mesma no escopo interno.
 """
 
 x = 1
 
 def escopo():
-    global x
+    # global x  # usar o global é considerado uma má pratica de programação
     x = 10
 
     def outra_funcao():  # As defs dentro das defs sempre seram executadas primeiro
-
+        # global x
         x = 11
         y = 2
         print(x, y)
