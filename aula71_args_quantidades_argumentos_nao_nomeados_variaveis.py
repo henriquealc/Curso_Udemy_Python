1f46c7746d5e37e1e3aca780e60099af2681c999
Henrique <aaluiziohenrique22@gmail.com>
2023-06-24 18:04:20 -0300

    Args quantidades de argumentos não nomeados variaveis

--- aula71_args_quantidades_argumentos_nao_nomeados_variaveis.py
+++ aula71_args_quantidades_argumentos_nao_nomeados_variaveis.py
@@ -6,8 +6,8 @@ args -> Argumentos não nomeados
 # Lembre-te de desempacotamento
 
 #              x, y|(resto)
-x, y, *resto = 1, 2, 3, 4
-print(x, y, resto)
+# x, y, *resto = 1, 2, 3, 4
+# print(x, y, resto)
 
 # def soma(x, y):
     # return x + y
@@ -15,10 +15,22 @@ print(x, y, resto)
 def soma (*args):
     total = 0
     for numero in args:
-        print('Total', total, numero)
-        total = total + numero
-        print('Total', total)
-    print('Total final: ', total)
+      total += numero
+    return total
 
 
-soma(1, 2, 3, 4, 5, 6)
+soma_1_2_3 = soma(1, 2, 3)
+print('Soma 1 + 2 + 3 = ', soma_1_2_3)
+
+soma_4_5_6 = soma(4, 5, 6)
+print('Soma 4 + 5 + 6 = ', soma_4_5_6)
+
+soma_2_3 = soma(1, 2)
+print('Soma 1 + 2 = ', soma_2_3)
+
+numeros = 1, 2, 3, 4, 5, 6, 7, 78, 10
+outra_soma = soma(*numeros)
+print('Resulado da soma de 1 até 7 + 78 + 10 = ', outra_soma)
+
+print('Soma usando a função "sum" =', end=' ')
+print(sum(numeros))
