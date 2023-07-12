5f6fddf93507dd6b00c0d77f3d346e67a9806379
Henrique <aaluiziohenrique22@gmail.com>
2023-07-12 18:12:19 -0300

    lista de compra
    Mudança no import os

--- aula54_Ex_lista_compras.py
+++ aula54_Ex_lista_compras.py
@@ -7,53 +7,53 @@ erros de índices inexistentes na lista.
 """
 
 '''RESOLUÇÃO FEITA POR MIM'''
-import os
-lista = [] # Sempre criar a variavel lista fora da estrutura de repetição
+# import os
+# lista = [] # Sempre criar a variavel lista fora da estrutura de repetição
 
 
-while True:
-    opcao = input('Selecione uma opção: \n[i]nserir [a]apagar [l]istar s[air]: ').lower()
-    if opcao == 'i':
-        os.system('cls')
-        inserir = input('Valor: ')
-        lista.append(inserir)
-    elif opcao == 'a':
-        os.system('cls')
-        apagar = input('Digite o indice que deseja apagar: ')
+# while True:
+#     opcao = input('Selecione uma opção: \n[i]nserir [a]apagar [l]istar s[air]: ').lower()
+#     if opcao == 'i':
+#         os.system('cls')
+#         inserir = input('Valor: ')
+#         lista.append(inserir)
+#     elif opcao == 'a':
+#         os.system('cls')
+#         apagar = input('Digite o indice que deseja apagar: ')
         
-        try:
-            indice = int(apagar)
-            del lista[indice] # deletar um indice da lista
+#         try:
+#             indice = int(apagar)
+#             del lista[indice] # deletar um indice da lista
         
-         # informa o erro
-        except ValueError:
-            print('Por favor digite número int.')
-        except IndexError:
-            print('Índice não existe na lista')
-        except Exception: # Feito para tratar algum erro desconhecido
-            print('Erro desconhecido')
-    elif opcao == 'l':
-        os.system('cls')
-        print(lista) # Informa a lista e os itens que contém nela
+#          # informa o erro
+#         except ValueError:
+#             print('Por favor digite número int.')
+#         except IndexError:
+#             print('Índice não existe na lista')
+#         except Exception: # Feito para tratar algum erro desconhecido
+#             print('Erro desconhecido')
+#     elif opcao == 'l':
+#         os.system('cls')
+#         print(lista) # Informa a lista e os itens que contém nela
         
-        if len(lista) == 0:
-            print('Nada para listar')
+#         if len(lista) == 0:
+#             print('Nada para listar')
         
-        for i, valor in enumerate(lista):
-            print(i, valor)
-        # mostra os indices da lista
-    elif opcao == 's':
-        print('Programa encerrado com sucesso, Obrigado!')
-        break
-    else:
-        print('Por favor escolha i, a ou l.')
+#         for i, valor in enumerate(lista):
+#             print(i, valor)
+#         # mostra os indices da lista
+#     elif opcao == 's':
+#         print('Programa encerrado com sucesso, Obrigado!')
+#         break
+#     else:
+#         print('Por favor escolha i, a ou l.')
 
 
 
 
 # Resolução feita pelo professor
 
-'''import os
+import os
 
 lista = []
 
@@ -62,7 +62,7 @@ while True:
     opcao = input('[i]nserir [a]pagar [l]istar: ')
 
     if opcao == 'i':
-        os.system('clear')
+        os.system('cls')
         valor = input('Valor: ')
         lista.append(valor)
     elif opcao == 'a':
@@ -80,7 +80,7 @@ while True:
         except Exception:
             print('Erro desconhecido')
     elif opcao == 'l':
-        os.system('clear')
+        os.system('cls')
 
         if len(lista) == 0:
             print('Nada para listar')
@@ -88,4 +88,5 @@ while True:
         for i, valor in enumerate(lista):
             print(i, valor)
     else:
-        print('Por favor, escolha i, a ou l.')'''
+        print('Por favor, escolha i, a ou l.')
+
