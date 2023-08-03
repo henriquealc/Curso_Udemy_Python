bff56dcace4f9dd828c5a9c020e128c6683fbda0
Henrique <aaluiziohenrique22@gmail.com>
2023-08-03 16:23:44 -0300

    Update aula85_list_comprehension_flat.py

--- list_comprehension.py/aula85_list_comprehension_flat.py
+++ list_comprehension.py/aula85_list_comprehension_flat.py
@@ -1,4 +1,5 @@
 numeros = [[numero, numero ** 2] for numero in range(10)]
-flat = [y for x in numeros for y in x]
+flat = [y for x in numeros for y in x] # Coloca tudo em uma lista só
 print(numeros)
-print(flat)
+print()
+print('Flat:', flat)
