44d1aa404f2eba08870e335444edd201de400dd2
Henrique <aaluiziohenrique22@gmail.com>
2023-08-27 02:50:10 -0300

    Modularização

---
+++ Python_Intermediario_Funcoes,Dicionarios,Modulos_e_+/aula97_m.py
@@ -0,0 +1,15 @@
+# Teste feito por mim para teste de importação
+# num = int(input('num1: '))
+# num2 = int(input('num2: '))
+
+# s = num + num2
+# print(s)
+
+
+# Ex usado pelo professor
+print('Este módulo se chama', __name__)
+
+variavel_modulo = 'Luiz'
+
+def soma(x, y):
+    return x + y
