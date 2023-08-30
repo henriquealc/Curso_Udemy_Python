55ba61572a7bccbeda926c22e74bd8e572843524
Henrique <aaluiziohenrique22@gmail.com>
2023-08-30 15:13:12 -0300

    Introdução a package

---
+++ Python_Intermediario_Funcoes,Dicionarios,Modulos_e_+/aula99_intro_package.py
@@ -0,0 +1,22 @@
+# Formas diferentes de importar os package
+# Apenas o import packtage NÃO FAZ NADA
+
+from aula99_package.modulo import soma_valores
+print(soma_valores(1, 2))
+
+import aula99_package.modulo
+print(aula99_package.modulo.soma_valores(1,2))
+
+from aula99_package import modulo
+print(modulo.soma_valores(2, 1))
+
+# Considerado má prática de programação
+# '*' se chama all
+from aula99_package.modulo import *
+print(soma_valores(1, 2))
+
+
+
+# Usando o all
+print(variavel)
+print(nova_variavel)
