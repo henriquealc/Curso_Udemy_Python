9556e7ae5afdf47f45ef5b1c03d59ac3c27a42dd
Henrique <aaluiziohenrique22@gmail.com>
2023-08-30 15:13:47 -0300

    Importando modulos

---
+++ Python_Intermediario_Funcoes,Dicionarios,Modulos_e_+/aula99_package/modulo.py
@@ -0,0 +1,19 @@
+# Quando precisar usar o all deve rever a aula
+# Colocado dentro do all as coisas que quero que import 
+__all__ = [
+    'variavel',
+    'soma_valores',
+    'nova_variavel',
+]
+
+from modulo_b import fala_oi
+
+
+variavel = 'Alguma coisa'
+
+
+nova_variavel = 'OK'
+
+
+def soma_valores (x, y):
+    return x + y
