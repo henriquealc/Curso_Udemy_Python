7a64986051ec7e2c2cc9ca284159b226b79e3549
Henrique <aaluiziohenrique22@gmail.com>
2023-08-31 02:24:18 -0300

    Como usar package

--- Python_Intermediario_Funcoes,Dicionarios,Modulos_e_+/aula99_intro_package.py
+++ Python_Intermediario_Funcoes,Dicionarios,Modulos_e_+/aula99_intro_package.py
@@ -1,25 +1,30 @@
 # Formas diferentes de importar os package
 # Apenas o import packtage NÃO FAZ NADA
 
-from aula99_package.modulo import soma_valores
-print(soma_valores(1, 2))
-
-import aula99_package.modulo
-print(aula99_package.modulo.soma_valores(1,2))
-
-from aula99_package import modulo
-print(modulo.soma_valores(2, 1))
-
-# Considerado má prática de programação
-# '*' se chama all
-from aula99_package.modulo import *
-print(soma_valores(1, 2))
-
-
-
-# Usando o all
-print(variavel)
-print(nova_variavel)
+# from aula99_package.modulo import soma_valores
+# print(soma_valores(1, 2))
 
 # import aula99_package.modulo
-# print(soma_valores(2, 5))
+# print(aula99_package.modulo.soma_valores(1,2))
+
+# from aula99_package import modulo
+# print(modulo.soma_valores(2, 1))
+
+# # Considerado má prática de programação
+# # '*' se chama all
+# from aula99_package.modulo import *
+# print(soma_valores(1, 2))
+
+
+
+# # Usando o all
+# print(variavel)
+# print(nova_variavel)
+
+# print(__name__)
+
+# Usa o import do __init__
+from aula99_package import fala_oi, soma_valores
+
+print(soma_valores(2, 3))
+fala_oi()
