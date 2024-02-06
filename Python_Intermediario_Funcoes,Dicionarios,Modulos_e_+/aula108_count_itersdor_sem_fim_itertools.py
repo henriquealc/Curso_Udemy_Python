459c9fac926b68f02c1000c36b9616ba03c45f48
Henrique <aaluiziohenrique22@gmail.com>
2024-02-06 13:13:32 -0300

    Diferença entre count e range

--- Python_Intermediario_Funcoes,Dicionarios,Modulos_e_+/aula108_count_itersdor_sem_fim_itertools.py
+++ Python_Intermediario_Funcoes,Dicionarios,Modulos_e_+/aula108_count_itersdor_sem_fim_itertools.py
@@ -6,15 +6,14 @@
 '''Comparando o count com o range'''
 
 
-
 from itertools import count
 
 #count(inicio, multiplo)
 #count pode passar argumentos nomeados start = começa/ step= pula de 8 em 8
-c1 = count(step= 8, start=0) # So pode passar so o inicio e multiplos
+c1 = count(step= 8, start=0) # Só se pode passar o inicio e multiplos
 
 #range(inicio, fim, multiplo)
-r1 = range(10, 100, 8)
+r1 = range(10, 100, 8) # Range eu sempre tenho que passar aqui um argumento onde quero que termine
 
 
 print('c1', hasattr(c1, '__iter__')) # Verifica se é um iteravel
@@ -25,12 +24,15 @@ print('r1', hasattr(r1, '__next__')) # O next não funciona no range
 print()
 print('count')
 for i in c1:
-    if i > 100: # Sem o if cria um loop infinito
+    if i > 100: # Sem o if cria um loop infinito e passo até onde quero que o count vá
         break
     print(i)
 
 print()
 
+# Exemplo de como o range funciona em comparação ao count
 print('Range')
 for i in r1:
     print(i)
+
+
